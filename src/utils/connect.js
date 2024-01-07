d97cc43a32ed72dfeae67e22dfd9138795bd9bd1
charles_sin <charles_sin@superbtech.asia>
2024-01-07 22:02:49 +0800

    first commit

---
+++ src/utils/connect.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  // prisma = new PrismaClient().$extends(withAccelerate());
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    // global.prisma = new PrismaClient().$extends(withAccelerate());
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
---
+++ src/utils/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementid: process.env.MEASUREMENTID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
